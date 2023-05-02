726cb09dcbc90051c4043fc2010cfe6d1bf4ff9d
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-05-03 00:21:19 +0330

    roads added

--- cherry_test_example/cherry_app/models.py
+++ cherry_test_example/cherry_app/models.py
@@ -6,13 +6,13 @@ class Node:
 
 
 class Owner:
-    def __int__(self, name, national_code, age,
-                total_toll_paid, owner_car):
+    def __init__(self, name, national_code, age,
+                 total_toll_paid, cars):
         self.name = name
         self.national_code = national_code
         self.age = age
         self.total_toll_paid = total_toll_paid
-        self.cars = owner_car
+        self.cars = cars
 
 
 class Car:
@@ -30,3 +30,9 @@ class Toll:
         self.toll_per_cross = toll_per_cross
         self.location = location
 
+
+class Road:
+    def __init__(self, name, width, geom):
+        self.name = name
+        self.width = width
+        self.geom = geom
