e575f84b463bbe3d1fbb67f1ffa9bd89558d5924
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-05-02 22:26:39 +0330

    suitable classes which can include json information completed

--- cherry_test_example/cherry_app/models.py
+++ cherry_test_example/cherry_app/models.py
@@ -1,9 +1,26 @@
-from django.db import models
-
-
-# Create your models here.
-class Car:
+class Node:
     def __init__(self, car, location, date):
         self.car = car
         self.location = location
         self.date = date
+
+
+class Owner:
+    def __int__(self, name, national_code, age,
+                total_toll_paid, owner_car):
+        self.name = name
+        self.national_code = national_code
+        self.age = age
+        self.total_toll_paid = total_toll_paid
+        self.cars = owner_car
+
+
+class Car:
+    def __init__(self, cId, cType, color, length, load_volume):
+        self.id = cId
+        self.type = cType
+        self.color = color
+        self.length = length
+        self.load_volume = load_volume
+
+
