76f7106e55cb2e05bd1f9fd7cb81a340b33c8168
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-05-02 20:46:36 +0330

    Car model created

--- cherry_test_example/cherry_app/models.py
+++ cherry_test_example/cherry_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Car:
+    def __init__(self, car, location, date):
+        self.car = car
+        self.location = location
+        self.date = date
