f19378c13e6890b3ba300e2f3a8ee4d24337a896
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-05-02 22:36:05 +0330

    function to get all info from the three files created in which gets and stores all infos in one dictionary

--- cherry_test_example/cherry_app/views.py
+++ cherry_test_example/cherry_app/views.py
@@ -1,18 +1,62 @@
 import json
-from django.shortcuts import render
+from models import Node, Owner, Car, Toll
 
 
-def my_view(request):
-    with open('myapp/data/all_nodes.json') as f1:
-        all_nodes = json.load(f1)
+def get_all_info():
+    with open('data/all_nodes.json', 'r') as data:
+        all_nodes_json = json.load(data)
 
-    with open('myapp/data/owners.json') as f2:
-        owners = json.load(f2)
+    with open('data/owners.json', 'r') as data:
+        owners_json = json.load(data)
 
-    with open('myapp/data/tollStations.json') as f3:
-        toll_stations = json.load(f3)
+    with open('data/tollStations.json', 'r') as data:
+        toll_stations_json = json.load(data)
 
-    # pass the data to your template context
-    context = {'all_nodes': all_nodes, 'owners': owners, 'toll_stations': toll_stations}
+    nodes = []
+    owners = []
+    toll_stations = []
 
-    return render(request, 'my_template.html', context)
+    # TAKING OUT ALL THE NODES IN FILE 'all_nodes.json' and saving them in cache
+    for n in all_nodes_json:
+        node = Node(
+            car=n['car'],
+            location=n['location'],
+            date=n['date']
+        )
+        nodes.append(node)
+
+    # TRYING TO DO THE SAME THING FOR 'owners.json'
+    for o in owners_json:
+        cars = []
+        for c in o['ownerCar']:
+            car = Car(
+                cId=c['id'],
+                cType=c['type'],
+                color=c['color'],
+                length=c['length'],
+                load_volume=c['load_valume']
+            )
+            cars.append(car)
+
+        owner = Owner(
+            name=o['name'],
+            national_code=o['national_code'],
+            age=o['age'],
+            total_toll_paid=o['total_toll_paid'],
+            cars=cars
+        )
+        owners.append(owner)
+
+    #SAME FOR TOLL_STATIONS
+    for t in toll_stations_json:
+        toll = Toll(
+            name=t['name'],
+            toll_per_cross=t['toll_per_cross'],
+            location=t['location']
+        )
+        toll_stations.append(toll)
+
+
+    #RETURNING THE RESULT
+    all_info = {"all_nodes": nodes, "owners": owners, "tollStations": toll_stations}
+    return all_info
