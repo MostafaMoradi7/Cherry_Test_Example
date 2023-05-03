658df45ad7ca9b7f775af491cda1a1b55bd5efb2
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-05-03 22:36:08 +0330

    adding new urls to the django

--- cherry_test_example/cherry_test_example/urls.py
+++ cherry_test_example/cherry_test_example/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 
 from cherry_app import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', views.test),
+    path('', views.main),
+    path('red-blue-cars', views.get_red_blue_cars)
 ]
