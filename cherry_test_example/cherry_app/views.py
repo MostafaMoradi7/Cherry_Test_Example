fc1a00bdab2c7db10637c1cdbd5163763c3aef43
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-05-03 22:36:25 +0330

    completing needed functions

--- cherry_test_example/cherry_app/views.py
+++ cherry_test_example/cherry_app/views.py
@@ -5,5 +5,13 @@ information = funcs.get_all_info()
 
 
 def test(request):
-    msg = information['all_nodes']
+    msg = information['roads']
     return render(request, 'index.html', {"message": msg})
+
+
+def main(request):
+    return render(request, 'main.html')
+
+
+def get_red_blue_cars(request):
+    pass
